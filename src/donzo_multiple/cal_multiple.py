b07d3261632984360a84d85328431e316290d133
lsiwh37249 <lsiwh37249@gmail.com>
2024-07-17 11:18:54 +0900

    update func

--- src/donzo_multiple/cal_multiple.py
+++ src/donzo_multiple/cal_multiple.py
@@ -1,6 +1,13 @@
 import sys
 
-a = int(sys.argv[1])
-b = int(sys.argv[2])
+def multiple(a,b):
+    print(a*b)
 
-print(a*b)             
+def main():
+
+    a = int(sys.argv[1])
+    b = int(sys.argv[2])
+
+    multiple(a,b)
+if __name__ == "__main__":
+    main()          
